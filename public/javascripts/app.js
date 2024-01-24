f46ee8a4db2d5afd53ff0cff7739f619a4e98741
Yanisdiga <gaidiyanis@gmail.com>
2024-01-24 19:40:57 +0100

    v1.3 ("Query Optimization")

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -35,158 +35,176 @@ groupButton.forEach((button) => {
 //Utilisation de proxy pour bypass le CORS
 const prefix = "http://localhost:8080/";
 
-function edtLoad() {
-    fetch(`${prefix}https://edt.univ-evry.fr/icsetudiant/${group}_etudiant(e).ics`)
-        .then(response => response.text())
-        .then(data => {
-            const jcalData = ICAL.parse(data);
-            const comp = new ICAL.Component(jcalData);
-            const vevents = comp.getAllSubcomponents('vevent');
+const edtURL = `https://edt-api.obstinate.fr/${group}` /*`${prefix}https://edt.univ-evry.fr/icsetudiant/${group}_etudiant(e).ics`*/
+const jsonURL = './name/L2INFO.json';
 
-            vevents.forEach((vevent) => {
-                const summary = vevent.getFirstPropertyValue('summary');
-                const dtstart = vevent.getFirstPropertyValue('dtstart');
-                const dtend = vevent.getFirstPropertyValue('dtend');
-                const dateStart = new Date(dtstart);
-                const dateEnd = new Date(dtend);
-                // Ajouter le décalage horaire UTC+1 (CET)
-                if (weekNumber >= 44 || weekNumber <= 13) {
-                    dateStart.setHours(dateStart.getHours() + 1);
-                    dateEnd.setHours(dateEnd.getHours() + 1);
-                }
-                else {
-                    dateStart.setHours(dateStart.getHours() + 2);
-                    dateEnd.setHours(dateEnd.getHours() + 2);
-                }
-                // Obtenir la date au format "YYYY-MM-DD"
-                const datePart = dateStart.toISOString().split("T")[0];
-                const dayOfWeek = dateStart.toLocaleDateString("fr-FR", { weekday: "long" });
-                // Obtenir l'heure (heures et minutes) au format "HH:mm"
-                const timePartStart = dateStart.toISOString().split("T")[1].substring(0, 5);
-                const timePartEnd = dateEnd.toISOString().split("T")[1].substring(0, 5);
-
-                const timeDifference = dateEnd.getTime() - dateStart.getTime();
-                const hours = Math.floor(timeDifference / (1000 * 60 * 60));
-                const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-                let timePartDuration = (hours + 1) * 3;
-
-                const year = dateStart.getFullYear();
-                const month = dateStart.getMonth() + 1;
-                Date.prototype.getWeekNumber = function () {
-                    const currentDate = new Date(this);
-                    currentDate.setHours(0, 0, 0, 0);
-                    currentDate.setDate(currentDate.getDate() + 4 - (currentDate.getDay() || 7));
-                    const yearStart = new Date(currentDate.getFullYear(), 0, 1);
-                    const weekNumber = Math.ceil(((currentDate - yearStart) / 86400000 + 1) / 7);
-                    return weekNumber;
-                };
-                const week = dateStart.getWeekNumber();
-                const location = vevent.getFirstPropertyValue('location');
-                const description = vevent.getFirstPropertyValue('description');
-                const teacher = description.split("\n")[1];
-
-                const displayContainer = document.querySelector('.display');
-                const lessonContainer = document.createElement('div');
-                lessonContainer.classList.add('lesson');
-                const lessonType = document.createElement('div');
-                lessonType.classList.add('lesson-type');
-                const lessonTitle = document.createElement('div');
-                lessonTitle.classList.add('lesson-title');
-                const lessonTeacher = document.createElement('div');
-                lessonTeacher.classList.add('lesson-teacher');
-                const lessonRoom = document.createElement('div');
-                lessonRoom.classList.add('lesson-room');
-
-                function removeLessons() {
-                    const lessons = document.querySelectorAll('.lesson');
-                    lessons.forEach((lesson) => {
-                        lesson.remove();
-                    });
-                }
-
-                removeLessons();
-                // Utilisation de la fonction fetchCourseData pour obtenir les données JSON
-                let name = '';
-                fetch('./name/L2INFO.json')
-                    .then(response => response.json()) // Utilisez response.json() pour extraire les données JSON.
-                    .then(data => {
-                        const lessonNameJson = data.code;
-                        const lessonName = summary.split(" ")[0];
-                        lessonNameJson.forEach((lesson) => {
-                            if (lesson[lessonName] != undefined) {
-                                if (lesson[lessonName] == "ALGEBRE ET ARTITHMETIQUE 1") {
-                                    if (location !== "1CY-1-A102") {
-                                        name = "THEORIE DES LANGAGES";
-                                    }
-                                }
-                                else {
-                                    name = lesson[lessonName];
-                                }
-                            }
-                        });
-                        if (name == '') {
-                            name = summary;
-                        }
-                        if (name == ' - indéfini') {
-                            name = "JVE-TVE";
-                        }
-
-                        const hours = document.querySelectorAll('.hour');
-                        hours.forEach((hour) => {
-                            const timeStart = parseInt(timePartStart.substring(0, 2), 10) + "h" + timePartStart.substring(3, 5);
-                            if (week == weekNumber && year == yearNumber && (hour.id == timeStart)) {
-                                lessonContainer.style.gridRowStart = parseInt(hour.dataset.row);
-                                lessonContainer.style.gridRowEnd = parseInt(hour.dataset.row) + timePartDuration;
-                                //lessonContainer.style.gridRowEnd = parseInt(hour.dataset.row) + 3;
-                                if (dayOfWeek == 'lundi') {
-                                    lessonContainer.style.gridColumn = 2;
-                                }
-                                if (dayOfWeek == 'mardi') {
-                                    lessonContainer.style.gridColumn = 3;
-                                }
-                                if (dayOfWeek == 'mercredi') {
-                                    lessonContainer.style.gridColumn = 4;
-                                }
-                                if (dayOfWeek == 'jeudi') {
-                                    lessonContainer.style.gridColumn = 5;
-                                }
-                                if (dayOfWeek == 'vendredi') {
-                                    lessonContainer.style.gridColumn = 6;
-                                }
-                                displayContainer.appendChild(lessonContainer);
-                                lessonType.innerHTML = summary.split(" ")[2] + ' - ' + '<span>' + timePartStart + ' - ' + timePartEnd + '</span>';
-                                if (summary.split(" ")[2] == 'TD') {
-                                    lessonContainer.style.backgroundColor = '#9B7E00';
-                                }
-                                if (summary.split(" ")[2] == 'TP') {
-                                    lessonContainer.style.backgroundColor = '#00FF00';
-                                }
-                                if (summary.split(" ")[2] == 'CM') {
-                                    lessonContainer.style.backgroundColor = '#0022A2';
-                                }
-                                if(summary.split(" ")[2] == 'DS' || summary.split(" ")[2] == 'Examen') {
-                                    lessonContainer.style.backgroundColor = '#A20000';
-                                }
-                                lessonTeacher.textContent = teacher;
-                                lessonRoom.textContent = "Salle: " + location;
-                                lessonTitle.textContent = name;
-
-                                lessonContainer.appendChild(lessonType);
-                                lessonContainer.appendChild(lessonTitle);
-                                lessonContainer.appendChild(lessonTeacher);
-                                lessonContainer.appendChild(lessonRoom);
-                            }
-                        });
-                    })
-                    .catch(error => {
-                        console.error('Erreur lors de la récupération des données JSON :', error);
-                    });
-
-
-            });
-        })
-        .catch(error => console.log(error));
+async function fetchData(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Erreur lors de la requête : ${response.status}`);
+    }
+    return response.text();
 }
+
+async function fetchJson(url) {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Erreur lors de la requête : ${response.status}`);
+    }
+    return response.json();
+}
+
+function removeLessons() {
+    const lessons = document.querySelectorAll('.lesson');
+    lessons.forEach((lesson) => {
+        lesson.remove();
+    });
+}
+
+async function edtLoad() {
+    try {
+        // Charger les données depuis le fichier ICS
+        const data = await fetchData(`https://edt-api.obstinate.fr/${group}`);
+        // Analyser les données ICS
+        const jcalData = ICAL.parse(data);
+        const comp = new ICAL.Component(jcalData);
+        const vevents = comp.getAllSubcomponents('vevent');
+
+        // Supprimer les cours précédents
+        removeLessons();
+
+        // Charger les données JSON pour le nom des cours
+        const nameData = await fetchJson(jsonURL);
+
+        vevents.forEach((vevent) => {
+            const summary = vevent.getFirstPropertyValue('summary');
+            const dtstart = vevent.getFirstPropertyValue('dtstart');
+            const dtend = vevent.getFirstPropertyValue('dtend');
+            const dateStart = new Date(dtstart);
+            const dateEnd = new Date(dtend);
+            // Ajouter le décalage horaire UTC+1 (CET)
+            if (weekNumber >= 44 || weekNumber <= 13) {
+                dateStart.setHours(dateStart.getHours() + 1);
+                dateEnd.setHours(dateEnd.getHours() + 1);
+            }
+            else {
+                dateStart.setHours(dateStart.getHours() + 2);
+                dateEnd.setHours(dateEnd.getHours() + 2);
+            }
+            // Obtenir la date au format "YYYY-MM-DD"
+            const datePart = dateStart.toISOString().split("T")[0];
+            const dayOfWeek = dateStart.toLocaleDateString("fr-FR", { weekday: "long" });
+            // Obtenir l'heure (heures et minutes) au format "HH:mm"
+            const timePartStart = dateStart.toISOString().split("T")[1].substring(0, 5);
+            const timePartEnd = dateEnd.toISOString().split("T")[1].substring(0, 5);
+
+            const timeDifference = dateEnd.getTime() - dateStart.getTime();
+            const hour = Math.floor(timeDifference / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            let timePartDuration = (hour + 1) * 3;
+
+            const year = dateStart.getFullYear();
+            const month = dateStart.getMonth() + 1;
+            Date.prototype.getWeekNumber = function () {
+                const currentDate = new Date(this);
+                currentDate.setHours(0, 0, 0, 0);
+                currentDate.setDate(currentDate.getDate() + 4 - (currentDate.getDay() || 7));
+                const yearStart = new Date(currentDate.getFullYear(), 0, 1);
+                const weekNumber = Math.ceil(((currentDate - yearStart) / 86400000 + 1) / 7);
+                return weekNumber;
+            };
+            const week = dateStart.getWeekNumber();
+            const location = vevent.getFirstPropertyValue('location');
+            const description = vevent.getFirstPropertyValue('description');
+            const teacher = description.split("\n")[1];
+
+            const displayContainer = document.querySelector('.display');
+            const lessonContainer = document.createElement('div');
+            lessonContainer.classList.add('lesson');
+            const lessonType = document.createElement('div');
+            lessonType.classList.add('lesson-type');
+            const lessonTitle = document.createElement('div');
+            lessonTitle.classList.add('lesson-title');
+            const lessonTeacher = document.createElement('div');
+            lessonTeacher.classList.add('lesson-teacher');
+            const lessonRoom = document.createElement('div');
+            lessonRoom.classList.add('lesson-room');
+
+            let name = '';
+            const lessonNameJson = nameData.code;
+            const lessonName = summary.split(" ")[0];
+            lessonNameJson.forEach((lesson) => {
+                if (lesson[lessonName] != undefined) {
+                    if (lesson[lessonName] == "ALGEBRE ET ARTITHMETIQUE 1") {
+                        if (location !== "1CY-1-A102") {
+                            name = "THEORIE DES LANGAGES";
+                        }
+                    }
+                    else {
+                        name = lesson[lessonName];
+                    }
+                }
+            });
+            if (name == '') {
+                name = summary;
+            }
+            if (name == ' - indéfini') {
+                name = "JVE-TVE";
+            }
+
+            const hours = document.querySelectorAll('.hour');
+            hours.forEach((hour) => {
+                const timeStart = parseInt(timePartStart.substring(0, 2), 10) + "h" + timePartStart.substring(3, 5);
+                if (week == weekNumber && year == yearNumber && (hour.id == timeStart)) {
+                    lessonContainer.style.gridRowStart = parseInt(hour.dataset.row);
+                    lessonContainer.style.gridRowEnd = parseInt(hour.dataset.row) + timePartDuration;
+                    //lessonContainer.style.gridRowEnd = parseInt(hour.dataset.row) + 3;
+                    if (dayOfWeek == 'lundi') {
+                        lessonContainer.style.gridColumn = 2;
+                    }
+                    if (dayOfWeek == 'mardi') {
+                        lessonContainer.style.gridColumn = 3;
+                    }
+                    if (dayOfWeek == 'mercredi') {
+                        lessonContainer.style.gridColumn = 4;
+                    }
+                    if (dayOfWeek == 'jeudi') {
+                        lessonContainer.style.gridColumn = 5;
+                    }
+                    if (dayOfWeek == 'vendredi') {
+                        lessonContainer.style.gridColumn = 6;
+                    }
+                    displayContainer.appendChild(lessonContainer);
+                    lessonType.innerHTML = summary.split(" ")[2] + ' - ' + '<span>' + timePartStart + ' - ' + timePartEnd + '</span>';
+                    if (summary.split(" ")[2] == 'TD') {
+                        lessonContainer.style.backgroundColor = '#9B7E00';
+                    }
+                    if (summary.split(" ")[2] == 'TP') {
+                        lessonContainer.style.backgroundColor = '#00FF00';
+                    }
+                    if (summary.split(" ")[2] == 'CM') {
+                        lessonContainer.style.backgroundColor = '#0022A2';
+                    }
+                    if (summary.split(" ")[2] == 'DS' || summary.split(" ")[2] == 'Examen') {
+                        lessonContainer.style.backgroundColor = '#A20000';
+                    }
+                    lessonTeacher.textContent = teacher;
+                    lessonRoom.textContent = "Salle: " + location;
+                    lessonTitle.textContent = name;
+
+                    lessonContainer.appendChild(lessonType);
+                    lessonContainer.appendChild(lessonTitle);
+                    lessonContainer.appendChild(lessonTeacher);
+                    lessonContainer.appendChild(lessonRoom);
+                }
+            });
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
 
 const nextWeek = document.getElementById('next-week');
 const previousWeek = document.getElementById('previous-week');
